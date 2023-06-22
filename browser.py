959f70546b33b3b24c5baccb230d95bcd094aa33
minty <p_a_v_l_u_s_h_a@mail.ru>
2023-06-22 23:33:09 +0300

    yes

--- browser.py
+++ browser.py
@@ -4,8 +4,9 @@ from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.action_chains import ActionChains # импортируем класс ActionChains из модуля action_chains
+
 # import pyscreenshot as ps
-# from main import click
 
 driver = webdriver.Firefox()
 options = Options()
@@ -19,42 +20,47 @@ index = 1
 index = "tile" + str(index)
 print("Ищем элемент с ID:", index)
 
-try:
-    element = WebDriverWait(driver, 10).until(
-    EC.presence_of_element_located((By.ID, index))
-    )
-except TimeoutException:
-    print("Элемент с ID '{}' не найден за 10 секунд.".format(index))
-    
-# element = driver.find_element(By.ID,"tile1")
-  
-
-# click(element,driver)
-'''
-element = driver.find_element(By.ID, index) 
 
 
+
+def click (element,driver):
+    actions = ActionChains(driver) 
+    actions.move_to_element(element) 
+    actions.click() 
+    actions.perform()
 
 def get_matrix(weight,hight):
     #array[weight][hight]
     array = [[0]* weight for i in range(hight)]
     for x in range(weight):
         for y in range(hight):
-            
             index = x * weight + y
             index = "tile"+str(index)
             element = driver.find_element(By.ID,index)
-            array[weight][hight] = element
-    for x in range(weight):
-        for y in range(hight):
-            print(array[x][y],end = " ")
-        print()
-    
+            array[x][y] = element
+    with open("test.txt","w")as f:    
+        for x in range(weight):
+            for y in range(hight):
+                f.write(str(array[x][y]))
+                f.write(" ")
+            f.write('\n')
+        
 
+
+try:
+    element = WebDriverWait(driver, 5).until(
+    EC.presence_of_element_located((By.ID, index))
+    )
+except TimeoutException:
+    print("Элемент с ID '{}' не найден за 10 секунд.".format(index))
+    
+element = driver.find_element(By.ID,"tile1")
+  
+
+click(element,driver)
+ 
 
 get_matrix(9,9)
 
 
-
-driver.quit()
-'''
+# driver.quit()
